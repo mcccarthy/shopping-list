7fd380820e467c22f8c36e61437de91edd2ff081
James McCarthy <mccarthyjames35@yahoo.com>
2023-08-28 08:22:18 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+/** @format */
+
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
