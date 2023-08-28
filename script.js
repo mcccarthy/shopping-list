a6e3407103d860e35694ff2b96e5a0d01a953ac6
James McCarthy <mccarthyjames35@yahoo.com>
2023-08-28 10:03:29 -0700

    filter is now working

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
 	e.preventDefault();
@@ -21,7 +22,10 @@ function addItem(e) {
 
 	li.appendChild(button);
 
+	// add li to the dom
 	itemList.appendChild(li);
+
+	checkUI();
 
 	itemInput.value = '';
 }
@@ -42,16 +46,51 @@ function createIcon(classes) {
 
 function removeItem(e) {
 	if (e.target.parentElement.classList.contains('remove-item')) {
-		e.target.parentElement.parentElement.remove();
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+
+			checkUI();
+		}
 	}
 }
 
 function clearItem() {
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild)
-  }
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild);
+	}
+	checkUI();
 }
+
+function filterItem(e) {
+	const items = itemList.querySelectorAll('li');
+	const text = e.target.value.toLowerCase();
+
+	items.forEach((item) => {
+		const itemName = item.firstChild.textContent.toLowerCase();
+
+		if (itemName.indexOf(text) != -1) {
+			item.style.display = 'flex';
+		} else {
+			item.style.display = 'none';
+		}
+	});
+}
+
+function checkUI() {
+	const items = itemList.querySelectorAll('li');
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		itemFilter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		itemFilter.style.display = 'block';
+	}
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItem);
+itemFilter.addEventListener('input', filterItem);
+
+checkUI();
